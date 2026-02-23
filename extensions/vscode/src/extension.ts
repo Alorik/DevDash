7f0d0e507ed048393081107c8aafe59ee85f8e7b
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-02-24 01:06:45 +0530

    fix: remove .next and node_modules from tracking

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -1,10 +1,15 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log("🚀 DevsDash Tracker Activated");
+  console.log("🚀 DevsDash Activated");
 
-  vscode.window.showInformationMessage("DevsDash Extension Running");
+  vscode.window.showInformationMessage("🔥 DevsDash is running");
+
+  const command = vscode.commands.registerCommand("devdash.start", () => {
+    vscode.window.showInformationMessage("DevsDash command executed");
+  });
+
+  context.subscriptions.push(command);
 }
 
 export function deactivate() {}
-
