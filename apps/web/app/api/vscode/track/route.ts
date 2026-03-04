6cac50ee5dd1a49b43a3c51ba2d010ac9fadad07
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-03-04 23:58:25 +0530

    feat: codingSessionSchema created and errors fixed mongodb connected

--- apps/web/app/api/vscode/track/route.ts
+++ apps/web/app/api/vscode/track/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
   const sessions = body.sessions;
 
   await CodingSession.insertMany(sessions);
-
+console.log("hola")
   return Response.json({ success: true });
 }
 
