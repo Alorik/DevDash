b8fadd52357701d853eedb8e77f155c353086079
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-03-07 23:26:54 +0530

    fix: allow borderRadius 0 and correct preview radius behavior

--- apps/web/app/api/vscode/stats/route.ts
+++ apps/web/app/api/vscode/stats/route.ts
@@ -1,7 +1,29 @@
+import { connectDb } from "@/lib/db/mongodb";
+import CodingSession from "@/models/CodingSession";
+
 type Stats = {
   todayCodingTime: Number;
   weekCodingTime: Number;
   monthCodingTime: Number;
 };
 
+export async function GET() {
+  await connectDb();
+  const sessions = await CodingSession.find().lean();
 
+  const startOfToday = new Date();
+  startOfToday.setHours(0, 0, 0, 0);
+
+  const startTimestamp = startOfToday.getTime();
+
+  const todaySessions = sessions.filter(
+    (session: any) => session.startTime >= startTimestamp,
+  );
+
+  const totalMilliseconds = todaySessions.reduce(
+    (sum: number, session: any) => sum + session.duration,
+    0,
+  );console.log("")
+  const todayHours = totalMilliseconds / (1000 * 60 * 60);
+  return Response.json(todayHours);
+}
