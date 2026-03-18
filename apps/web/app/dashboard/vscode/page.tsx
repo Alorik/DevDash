4476234f40bf54c396571103fea87674380eec9b
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-03-18 23:53:07 +0530

    fix: UI updated of enhancer page

--- apps/web/app/dashboard/vscode/page.tsx
+++ apps/web/app/dashboard/vscode/page.tsx
@@ -30,7 +30,7 @@ export default function DashboardPage() {
     <>
       <div className="p-10">
         <h1 className="text-3xl font-bold mb-8">DevsDash</h1>
-
+{/*  */}
         {/* Stats Cards */}
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
           <div className="p-6 border rounded-xl shadow">
