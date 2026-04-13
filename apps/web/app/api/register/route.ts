e83725111d3922964666742e81848fb8eebbef89
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-04-13 09:53:45 +0530

    User register api added

---
+++ apps/web/app/api/register/route.ts
@@ -0,0 +1,54 @@
+import { connectDb } from "@/lib/db/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/User";
+import bcrypt from "bcrypt";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDb();
+    const { name, email, password } = await req.json();
+
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { error: "All fields are required" },
+        { status: 400 },
+      );
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 409 },
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return NextResponse.json(
+      {
+        message: "user is registered Successfully",
+        useR: {
+          id: newUser._id,
+          name: newUser.name,
+          email: newUser.email,
+        },
+      },
+      {
+        status: 201,
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
