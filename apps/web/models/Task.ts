33a236e595534f379a1173ccc218b580f8e2cb1b
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-04-15 23:57:53 +0530

    Task schema added

---
+++ apps/web/models/Task.ts
@@ -0,0 +1,28 @@
+// models/task.ts
+
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Task = mongoose.models.Task || mongoose.model("Task", TaskSchema);
