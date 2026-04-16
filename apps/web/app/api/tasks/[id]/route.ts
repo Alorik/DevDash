d516ca067fde1e66e2b564817a12a470f84afec7
NITIN KIROLA <nitinkirola1701@gmail.com>
2026-04-16 06:10:11 +0530

    Task Deletion api completed

--- apps/web/app/api/tasks/[id]/route.ts
+++ apps/web/app/api/tasks/[id]/route.ts
@@ -5,12 +5,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
     await connectDb();
     const body = await req.json();
-    const { id: taskId } = await params; 
+    const { id: taskId } = await params;
 
     const { title, completed } = body;
     const updateData: any = {};
@@ -33,6 +33,37 @@ export async function PATCH(
     console.log(err);
     return NextResponse.json(
       { error: "Failed to update task" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    await connectDb();
+    const { id: taskId } = await params;
+
+    if (!mongoose.Types.ObjectId.isValid(taskId)) {
+      return NextResponse.json({ error: "Invalid TaskId" }, { status: 400 });
+    }
+
+    const deleteTask = await Task.findByIdAndDelete(taskId);
+
+    if (!deleteTask) {
+      return NextResponse.json({ error: "Task is not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: "Task deleted successfully" },
+      { status: 200 },
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "Failed to delete task" },
       { status: 500 },
     );
   }
